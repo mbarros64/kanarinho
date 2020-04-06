c6392b275b53decd71aa76ea0281abf275cd1af2
adrianotelesc <adriano.telesc@gmail.com>
2020-04-05 21:45:18 -0400

    Add cnpj validation

--- brazutils-core/src/main/java/com/adrianotelesc/brazutils/core/CpfNumberUtil.kt
+++ brazutils-core/src/main/java/com/adrianotelesc/brazutils/core/CpfNumberUtil.kt
@@ -2,6 +2,49 @@ package com.adrianotelesc.brazutils.core
 
 object CpfNumberUtil {
 
+    fun isValidCnpj(cnpjNumber: String): Boolean {
+        if (cnpjNumber.notContainsAny(FORMATTED_CNPJ_REGEX, UNFORMATTED_CNPJ_REGEX)) return false
+
+        val cnpjDigitsOnly =
+            if (cnpjNumber.matches(FORMATTED_CNPJ_REGEX)) cnpjNumber.removeNotDigits() else cnpjNumber
+
+        if (cnpjDigitsOnly.hasRepeatedDigits(numberOfRepeatedDigits = 10)) return false
+
+        val firstNineDigits = cnpjDigitsOnly.take(12)
+        val validFirstCheckDigit = calculateCnpjCheckDigitBy(firstNineDigits)
+
+        val firstCheckDigit = cnpjDigitsOnly[FIRST_CHECK_DIGIT_CNPJ_INDEX].toNumericValue()
+
+        if (firstCheckDigit != validFirstCheckDigit) return false
+
+        val firstTenDigits = cnpjDigitsOnly.take(13)
+        val validSecondCheckDigit = calculateCnpjCheckDigitBy(firstTenDigits)
+
+        val secondCheckDigit = cnpjDigitsOnly[SECOND_CHECK_DIGIT_CNPJ_INDEX].toNumericValue()
+
+        if (secondCheckDigit != validSecondCheckDigit) return false
+
+        return true
+    }
+
+    private fun calculateCnpjCheckDigitBy(cpfDigits: String): Int {
+        if (cpfDigits.length !in 12..13)
+            throw IllegalArgumentException("Cannot calculate the check digit correctly")
+
+        val weights = arrayOf(6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2).reversed()
+
+        val reversedCpfDigits = cpfDigits.reversed()
+        val sum = reversedCpfDigits.sumIndexedBy { index, digit ->
+            val number = digit.toNumericValue()
+            val weight = weights[index]
+
+            number * weight
+        }
+
+        val mod11 = sum % 11
+        return if (mod11 < 2) 0 else 11 - mod11
+    }
+
     fun isValidCpf(cpfNumber: String): Boolean {
         if (cpfNumber.notContainsAny(FORMATTED_CPF_REGEX, UNFORMATTED_CPF_REGEX)) return false
 
@@ -11,14 +54,14 @@ object CpfNumberUtil {
         if (cpfDigitsOnly.hasRepeatedDigits(numberOfRepeatedDigits = 10)) return false
 
         val firstNineDigits = cpfDigitsOnly.take(9)
-        val validFirstCheckDigit = calculateCheckDigitBy(firstNineDigits)
+        val validFirstCheckDigit = calculateCpfCheckDigitBy(firstNineDigits)
 
         val firstCheckDigit = cpfDigitsOnly[FIRST_CHECK_DIGIT_INDEX].toNumericValue()
 
         if (firstCheckDigit != validFirstCheckDigit) return false
 
         val firstTenDigits = cpfDigitsOnly.take(10)
-        val validSecondCheckDigit = calculateCheckDigitBy(firstTenDigits)
+        val validSecondCheckDigit = calculateCpfCheckDigitBy(firstTenDigits)
 
         val secondCheckDigit = cpfDigitsOnly[SECOND_CHECK_DIGIT_INDEX].toNumericValue()
 
@@ -27,7 +70,7 @@ object CpfNumberUtil {
         return true
     }
 
-    private fun calculateCheckDigitBy(cpfDigits: String): Int {
+    private fun calculateCpfCheckDigitBy(cpfDigits: String): Int {
         if (cpfDigits.length !in 9..10)
             throw IllegalArgumentException("Cannot calculate the check digit correctly")
 
@@ -43,8 +86,12 @@ object CpfNumberUtil {
         return if (mod11 < 2) 0 else 11 - mod11
     }
 
-    private val FORMATTED_CPF_REGEX = Regex("""^\d{3}.\d{3}.\d{3}-\d{2}${'$'}""")
+    private val FORMATTED_CPF_REGEX = Regex("""^\d{3}.\d{3}.\d{3}-\d{2}$""")
+    private val FORMATTED_CNPJ_REGEX = Regex("""^\d{2}.\d{3}.\d{3}/\d{4}-\d{2}$""")
     private val UNFORMATTED_CPF_REGEX = Regex("""^\d{11}$""")
+    private val UNFORMATTED_CNPJ_REGEX = Regex("""^\d{14}$""")
     private const val FIRST_CHECK_DIGIT_INDEX = 9
     private const val SECOND_CHECK_DIGIT_INDEX = 10
+    private const val FIRST_CHECK_DIGIT_CNPJ_INDEX = 12
+    private const val SECOND_CHECK_DIGIT_CNPJ_INDEX = 13
 }
--- brazutils-core/src/test/java/com/adrianotelesc/brazutils/core/CpfNumberUtilTest.kt
+++ brazutils-core/src/test/java/com/adrianotelesc/brazutils/core/CpfNumberUtilTest.kt
@@ -219,4 +219,214 @@ class CpfNumberUtilTest {
         }
     }
 
+    @Nested
+    inner class IsValidCnpj {
+        @Test
+        fun `given empty cnpj, when verified if it's valid, then return false`() {
+            val cnpj = ""
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given blank cnpj, when verified if it's valid, then return false`() {
+            val cnpj = " "
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given invalid formatted cnpj, when verified if it's valid, then return false`() {
+            val cnpj = "11.222.333/0001-10"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given valid formatted cnpj, when verified if it's valid, then return true`() {
+            val cnpj = "11.222.333/0001-81"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertTrue(isValid)
+        }
+
+        @Test
+        fun `given bad formatted cnpj, when verified if it's valid, then return false`() {
+            val cnpj = "11 222 333 0001 81"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given formatted cnpj with less than 11 characters, when verified if it's valid, then return false`() {
+            val cnpj = "11.222.333/0001-8"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given formatted cnpj with greater than 11 characters, when verified if it's valid, then return false`() {
+            val cnpj = "11.222.333/0001-109"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given formatted cnpj with letters, when verified if it's valid, then return false`() {
+            val cnpj = "11.222.333/0001-jk"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given formatted cnpj with special characters, when verified if it's valid, then return false`() {
+            val cnpj = "123.456.789-%$"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given formatted cnpj plus letters, when verified if it's valid, then return false`() {
+            val cnpj = "11.222.333/0001-81jk"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given formatted cnpj plus special characters, when verified if it's valid, then return false`() {
+            val cnpj = "11.222.333/0001-81%$"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given formatted cnpj with whitespaces, when verified if it's valid, then return false`() {
+            val cnpj = "11.222.333/0001-  "
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given formatted cnpj plus whitespaces, when verified if it's valid, then return false`() {
+            val cnpj = "11.222.333/0001-81 "
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given invalid unformatted cnpj, when verified if it's valid, then return false`() {
+            val cnpj = "11222333000110"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun  `given valid unformatted cnpj, when verified if it's valid, then return true`() {
+            val cnpj = "11222333000181"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertTrue(isValid)
+        }
+
+        @Test
+        fun `given unformatted cnpj with less than 11 characters, when verified if it's valid, then return false`() {
+            val cnpj = "1234567890"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given unformatted cnpj with greater than 11 characters, when verified if it's valid, then return false`() {
+            val cnpj = "112223330001819"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given unformatted cnpj with letters, when verified if it's valid, then return false`() {
+            val cnpj = "112223330001jk"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given unformatted cnpj with special characters, when verified if it's valid, then return false`() {
+            val cnpj = "112223330001%$"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given unformatted cnpj plus letters, when verified if it's valid, then return false`() {
+            val cnpj = "11222333000181jk"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given unformatted cnpj plus special characters, when verified if it's valid, then return false`() {
+            val cnpj = "11222333000181%$"
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given unformatted cnpj with whitespaces, when verified if it's valid, then return false`() {
+            val cnpj = "112223330001  "
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+
+        @Test
+        fun `given unformatted cnpj plus whitespaces, when verified if it's valid, then return false`() {
+            val cnpj = "11222333000181 "
+
+            val isValid = CpfNumberUtil.isValidCnpj(cnpj)
+
+            assertFalse(isValid)
+        }
+    }
+
 }
